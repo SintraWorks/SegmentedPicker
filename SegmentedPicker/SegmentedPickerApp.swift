985a171cd3a2b1c4b1c6f3b5367c797d866cdcee
Antonio Nunes <antonionunes@me.com>
2020-07-07 13:47:26 +0200

    Initial Commit

---
+++ SegmentedPicker/SegmentedPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedPickerApp.swift
+//  SegmentedPicker
+//
+//  Created by Antonio Nunes on 07/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
